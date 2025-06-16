fbffa06705636f9a59f09f5e8f1aeed7ad52745f
wldwd <68261156+wldwd@users.noreply.github.com>
2025-06-16 20:58:48 +0800

    fix: copy about and contact pages into build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/PARKHOUND/",
   build: {
     sourcemap: true,
-  }
-}
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about.html",
+        contact: "contact.html",
+      },
+    },
+  },
+});
